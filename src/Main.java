5ec14965e3310f1daf889693cbb30b57156d70a8
esko9 <115159442+esko9@users.noreply.github.com>
2022-12-08 21:31:30 +0100

    f

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,13 @@ public class Main{
 
     public static void Zad2()
     {
-        Animal[] Animals = new Animal[7];
-        Animals[1] = new Mammal("Da", 4);
-        Animals[2] = new Bird("Red", 1, 0.5, "Red");
-        Animals[3] = new Dog("Dog", 4, 6, "York");
-        Animals[4] = new Pigeon("ds", 2, 0.3, "Blue", "flying", 21);
-        Animals[5] = new Fish("Fish", 1, "Salt Water");
-        Animals[6] = new Blowfish("Fish", 1, 1, "Salt Water");
+        Animal[] Animals = new Animal[6];
+        Animals[0] = new Mammal("Da", 4);
+        Animals[1] = new Bird("Red", 1, 0.5, "Red");
+        Animals[2] = new Dog("Dog", 4, 6, "York");
+        Animals[3] = new Pigeon("ds", 2, 0.3, "Blue", "flying", 21);
+        Animals[4] = new Fish("Fish", 1, "Salt Water");
+        Animals[5] = new Blowfish("Fish", 1, 1, "Salt Water");
 
 
         for (int i = 1; i < Animals.length; i++)
@@ -22,6 +22,7 @@ public class Main{
             Animals[i].getVoice();
             Animals[i].sleep();
             System.out.println(Animals[i].toString());
+            Animals[i].move();
         }
 
         AnimalBehavior[] animalBehaviors = new AnimalBehavior[3];
